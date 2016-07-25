47b386a5e323f53c6492d737eca43266e9676a2e
Priyank <Priyank@Lenovo-PC>
2016-07-25 22:21:36 +0530

    Removed the hello world again message - redundant

--- GitTutorial_Project/src/com/Aritha/Learning/gitViaEclipse/MainApplication.java
+++ GitTutorial_Project/src/com/Aritha/Learning/gitViaEclipse/MainApplication.java
@@ -5,7 +5,7 @@ public class MainApplication {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World");
-		System.out.println("Hello World Again!");
+		
 		
 		AgileGreetingClass AglGreeting = new AgileGreetingClass("Hello Agile World!");
 		AglGreeting.printAgileGreetingMsg();
